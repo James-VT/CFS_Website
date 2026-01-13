5742cca803e77116b5e30089295d3e144b0a07b6
James-VT <jd_tanner@hotmail.co.uk>
2026-01-13 20:17:14 +0000

    Adds react-bootstrap and a navbar

--- cfs-website/src/main.tsx
+++ cfs-website/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import HeaderNavbar from './components/HeaderNavbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <HeaderNavbar />
+    <h1>Welcome!</h1>
+  </>
 )
